6b43f08559ff066f1f187539ada669bc0b9ea862
Wanderson Fontes <41520210+WandersonFontes@users.noreply.github.com>
2022-06-21 08:08:44 -0300

    chore:remove secret key

--- games_store/settings.py
+++ games_store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=$*=bn0ct&e5cc9n^1i#n65kob03k3&-xt^ft$bn71yeww2x*l'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
